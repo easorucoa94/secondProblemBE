46e752e384dbcffbca96145e8464e579d79f79e9
root <root@easorucoa94-System-Product-Name>
2020-02-08 20:44:01 -0400

    - Add SpringBoot base structure

---
+++ secondProblem/src/main/java/com/truextend/secondProblem/SecondProblemApplication.java
@@ -0,0 +1,13 @@
+package com.truextend.secondProblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProblemApplication.class, args);
+	}
+
+}

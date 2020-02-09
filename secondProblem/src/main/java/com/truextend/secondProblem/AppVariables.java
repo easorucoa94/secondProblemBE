eae06e2e0a450bf00598c587e52354df1039baf6
root <root@easorucoa94-System-Product-Name>
2020-02-09 00:24:30 -0400

    Add application variables

---
+++ secondProblem/src/main/java/com/truextend/secondProblem/AppVariables.java
@@ -0,0 +1,14 @@
+package com.truextend.secondProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.truextend.secondProblem.entities.ClassEntity;
+import com.truextend.secondProblem.entities.StudentEntity;
+
+public class AppVariables {
+
+	public static final List<StudentEntity> studentList = new ArrayList<StudentEntity>();
+	public static final List<ClassEntity> classList = new ArrayList<ClassEntity>();
+
+}
